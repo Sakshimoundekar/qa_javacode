4e28f088f5d4486881d4116fff56f1874b2f215c
Sakshimoundekar <84739234+Sakshimoundekar@users.noreply.github.com>
2021-09-13 17:24:57 +0530

    Assignment1

---
+++ FirstDayJavaPGM/src/org/emp/EmployeeDetails.java
@@ -0,0 +1,38 @@
+package org.emp;
+
+public class EmployeeDetails {
+   
+	  public void empName() {
+		  System.out.println("Java");
+	  }
+	  public void empId() {
+		  System.out.println(1234);
+	  }
+	  public void empDob() {
+		  System.out.println(04.1999);
+	  }
+	  public void empPhone() {
+		  System.out.println(8308022456l);
+	  }
+	  public void empEmail() {
+		  System.out.println("sakshimoundekar@gmail.com");
+	  }
+	  public void empAdd() {
+		  System.out.println("Subedar lay out");
+	  }
+	  
+	  public static void main (String[] args) {
+		  
+		  EmployeeDetails emp =new EmployeeDetails();
+		  emp.empName();
+		  emp.empId();
+		  emp.empDob();
+		  emp.empPhone();
+		  emp.empEmail();
+		  emp.empAdd();
+		  
+		  
+	  }
+}
+	  
+	  
