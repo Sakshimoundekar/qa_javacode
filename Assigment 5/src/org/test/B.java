47e820c468098f8e7957bdf6847c05ac4fa790dd
Sakshimoundekar <84739234+Sakshimoundekar@users.noreply.github.com>
2021-09-17 12:49:15 +0530

    question5

--- Assigment 5/src/org/test/B.java
+++ Assigment 5/src/org/test/B.java
@@ -1,15 +1,8 @@
 package org.test;
 
 public class B {
-	public B() {	 
-		 System.out.println();	
-		 
-		 }
-	public B(int id) {
-		System.out.println(id);	
-		
-	
-
+	public B(int id ) {	 
+		 System.out.println(id);	
 	}
-}	
+}
 	
